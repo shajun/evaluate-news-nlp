f84130fe46b4092a4894e058a1238cf4ad7e8abb
shajun <junco.sha@gmail.com>
2020-02-19 12:43:41 +0800

    fix: Repair function

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,39 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-const app = express()
+var json = {
+  title: 'test json response',
+  message: 'this is a message',
+  time: 'now'
+};
 
-app.use(express.static('dist'))
+const app = express();
+app.use(cors());
+// to use json
+app.use(bodyParser.json());
+// to use url encoded values
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-console.log(__dirname)
+app.use(express.static('dist'));
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+console.log(JSON.stringify(mockAPIResponse));
+
+app.get('/', function(req, res) {
+  res.sendFile('dist/index.html');
+});
+
+app.get('/test', function(req, res) {
+  res.json(mockAPIResponse);
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.listen(8081, function() {
+  console.log('Example app listening on port 8081!');
+});
